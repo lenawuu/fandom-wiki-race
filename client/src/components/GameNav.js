12cf52bc7462f0a3a355f9c55b612baf40ec9dbd
lenawuu <lenawu24@gmail.com>
2024-09-28 16:51:28 -0500

    game start

--- client/src/components/GameNav.js
+++ client/src/components/GameNav.js
@@ -2,7 +2,7 @@ import { Button } from "@headlessui/react";
 import { ReactComponent as LeftArrowSVG } from "../assets/arrow-left.svg";
 import { ReactComponent as RightArrowSVG } from "../assets/arrow-right.svg";
 
-function GameNav() {
+function GameNav({ goal, numClicks }) {
   return (
     <nav>
       <div id="buttonGroup">
@@ -12,6 +12,14 @@ function GameNav() {
         <Button>
           <RightArrowSVG />
         </Button>
+        <div>
+          <p>
+            {goal.start.name} → {goal.end.name}
+          </p>
+        </div>
+        <div>
+          <p>Clicks: {numClicks}</p>
+        </div>
       </div>
     </nav>
   );
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,19 +1,25 @@
 import GameNav from "../components/GameNav";
+import IFrame from "../components/IFrame";
+import { useState } from "react";
 
 function Game() {
   const goal = {
-    start: "",
-    end: "",
+    start: {
+      name: "Leaves",
+      url: "https://minecraft.fandom.com/wiki/Leaves",
+    },
+    end: {
+      name: "Bone Meal",
+      url: "https://minecraft.fandom.com/wiki/Bone_Meal",
+    },
   };
+
+  const [numClicks, setNumClicks] = useState(0);
+
   return (
     <div class="w-screen h-screen">
-      <GameNav />
-      <div class="w-full h-full">
-        <iframe
-          class="w-full h-full"
-          src="https://minecraft.fandom.com/wiki/Minecraft_Wiki"
-        ></iframe>
-      </div>
+      <GameNav goal={goal} numClicks={numClicks} />
+      <IFrame />
     </div>
   );
 }
