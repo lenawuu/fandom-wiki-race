fd0d0350fd71653ed5b4a06a847fa54a97fbd249
dBCooper2 <t.rowland711@gmail.com>
2024-10-02 15:47:26 -0500

    test

--- flask-docker-example/wikiRaceBackend/app/routes/page_routes.py
+++ flask-docker-example/wikiRaceBackend/app/routes/page_routes.py
@@ -8,3 +8,8 @@ bp = Blueprint('page_routes', __name__)
 def homepage():
     # Render an HTML template (index.html) located in the templates folder
     return "Hello World!"
+
+@bp.route('/Game')
+def gamepage():
+    # Render the Game Start Page Here (Embedded Start Page)
+    return "This is the Game Page!"
