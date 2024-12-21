8c103f39caab59e10587c382ea6f1b49f30c6b94
dBCooper2 <t.rowland711@gmail.com>
2024-12-20 21:04:26 -0600

    updated schema and endpoints to match 3NF version of DB

--- flask-docker-example/flaskapp/app.py
+++ flask-docker-example/flaskapp/app.py
@@ -2,10 +2,11 @@
 # TODO: Change DB to match new schema
 # We need...
 #   - Get Game By ID
-#   -
+#   - Get All Games
 
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy.orm import relationship
 from flask_cors import CORS
 from os import environ
 
@@ -25,6 +26,8 @@ class User(db.Model):
     def json(self):
         return {'id':self.id, 'name':self.name, 'email':self.email}
 
+# REDO: CHANGE SCHEMA TO MATCH NEW VALS
+''' THIS IS THE OLD VERSION, NEW VERSION IS BELOW
 class Game(db.Model):
     __tablename__ = 'games'
     id = db.Column(db.Integer, primary_key=True)
@@ -32,8 +35,60 @@ class Game(db.Model):
     end = db.Column(db.String(100), unique=False, nullable=False)
     route = db.Column(db.String(200), unique=True, nullable=False)
 
+
     def json(self):
         return {'id':self.id, 'start':self.start, 'end':self.end, 'route':self.route}
+'''
+
+# NEW SCHEMA
+#   - Uses a 3NF Form to store arrays (Location is for start and end)
+
+class Game(db.model):
+    __tablenameA__ = 'games'
+    id = db.Column(db.Integer, primary_key=True)
+    fandom_domain = db.Column(db.String(100), nullable=False)
+    route_length = db.Column(db.Integer, nullable=False)
+
+    # Connected to the Locations Table via foreign Key
+    start_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    end_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    start = relationship("Location", foreign_keys=[start_id], backref="games_start")
+    end = relationship("Location", foreign_keys=[end_id], backref="games_end")
+
+    # Connect to the Route via a SQLAlchemy.orm.relationship method
+    route = relationship("Route", back_populates="game", cascade="all, delete-orphan")
+
+    # Return the new version of the Tables as a JSON:
+    def json(self):
+        return {
+            "id": self.id,
+            "start": {"title": self.start.title, "url": self.start.url},
+            "end": {"title": self.end.title, "url": self.end.url},
+            "route": {
+                "titles": [route.title for route in self.route],
+                "urls": [route.url for route in self.route]
+            },
+            "fandom_domain": self.fandom_domain,
+            "route_length": self.route_length
+        }
+
+# Locations Store the Start and Endpoints of a Game
+class Location(db.Model):
+    __tablename__ = 'locations'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    url = db.Column(db.String(500), nullable=False)
+
+# Routes Store a single route and are stored as a series of ID's in the Game Table,
+#   and a Route row in this table is just a URL used in the route
+class Route(db.Model):
+    __tablename__ = 'routes'
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    url = db.Column(db.String(500), nullable=False)
+
+    game = relationship("Game", back_populates="route")
 
 db.create_all()
 
@@ -43,6 +98,9 @@ db.create_all()
 #   - Get Game By ID (for pulling the game of the day, id will be 1)
 #   - Get All Games (for listing all previous games on the frontend)
 
+            ################################################################################################
+
+# HARD-CODED OUTPUTS FOR FRONTEND TESTING:
 test_game_1 = {
     "id": 1,
     "start": {
@@ -99,8 +157,9 @@ games = list()
 games.append(test_game_1)
 games.append(test_game_2)
 
-########################################################################################################################
+            ################################################################################################
 
+# TEST ROUTES - REPLACE IN FRONTEND
 @app.route('/api/game', methods=['GET'])
 def test_get_all_games():
     try:
@@ -137,37 +196,70 @@ def test_get_game_by_id(id):
 
 # CREATE GAME(S)::
 
+# New Version of Game Creation to Match the New Schema
+#   - This version checks if a Location or Route row exists in the db before creating it in the tables.
 @app.route('/api/game', methods=['POST'])
 def create_game():
     try:
         data = request.get_json(force=True)
-        new_game = Game(start=data['start'], end=data['end'], route=data['route'])
+
+        # Extract start and end locations
+        start_data = data['start']
+        end_data = data['end']
+
+        # Check if start and end locations exist; otherwise, create them
+        start = Location.query.filter_by(title=start_data['title'], url=start_data['url']).first()
+        if not start:
+            start = Location(title=start_data['title'], url=start_data['url'])
+            db.session.add(start)
+
+        end = Location.query.filter_by(title=end_data['title'], url=end_data['url']).first()
+        if not end:
+            end = Location(title=end_data['title'], url=end_data['url'])
+            db.session.add(end)
+
+        # Commit to get start and end IDs
+        db.session.flush()
+
+        # Create the game
+        new_game = Game(
+            start=start,
+            end=end,
+            fandom_domain=data['fandom_domain'],
+            route_length=data['route_length']
+        )
         db.session.add(new_game)
+        db.session.flush()
+
+        # Add route entries
+        for title, url in zip(data['route']['titles'], data['route']['urls']):
+            route_entry = Route(game_id=new_game.id, title=title, url=url)
+            db.session.add(route_entry)
+
+        # Commit everything
         db.session.commit()
 
-        return jsonify({
-            'id':new_game.id,
-            'start':new_game.start,
-            'end':new_game.end,
-            'route':new_game.route,
-        }), 201
+        return jsonify(new_game.json()), 201
 
     except Exception as e:
-        return make_response(jsonify({'message':'error creating game', 'error':str(e)}), 500)
+        db.session.rollback()  # Rollback in case of error
+        return make_response(jsonify({'message': 'error creating game', 'error': str(e)}), 500)
+
 
 
 # READ GAME(S)::
 
+# READ ALL GAMES
 @app.route('/api/game', methods=['GET'])
 def get_all_games():
     try:
         games = Game.query.all()
-        games_data = [{'id': game.id, 'start': game.start, 'end': game.end, 'route': game.route} for game in games] # put everything in a json
+        games_data = [game.json() for game in games] # 12/20/24 - updated to just create a list of the json() method
         return jsonify(games_data), 200
     except Exception as e:
         return make_response(jsonify({'message':'error getting games', 'error':str(e)}), 500)
 
-
+# READ GAME BY ID
 @app.route('/api/game/<id>', methods=['GET'])
 def get_game_by_id(id):
     try:
@@ -182,41 +274,69 @@ def get_game_by_id(id):
         return make_response(jsonify({'message':f'error getting game {id}', 'error':str(e)}), 500)
 
 # UPDATE GAME(S)::
-
 @app.route('/api/game/<id>', methods=['PUT'])
 def update_game_by_id(id):
     try:
         game = Game.query.filter_by(id=id).first()
 
-        if game:
-            data = request.get_json()
-            game.start = data['start']
-            game.end = data['end']
-            game.route = data['route']
-            db.session.commit()
-            return make_response(jsonify({'message':f'game {id} updated'}), 200)
+        if not game:
+            return make_response(jsonify({'message': f'game {id} not found'}), 404)
 
-        return make_response(jsonify({'message':f'game {id} not found'}), 404)
+        data = request.get_json()
+
+        # Update or create start location
+        start_data = data['start']
+        start = Location.query.filter_by(title=start_data['title'], url=start_data['url']).first()
+        if not start:
+            start = Location(title=start_data['title'], url=start_data['url'])
+            db.session.add(start)
+        game.start = start
+
+        # Update or create end location
+        end_data = data['end']
+        end = Location.query.filter_by(title=end_data['title'], url=end_data['url']).first()
+        if not end:
+            end = Location(title=end_data['title'], url=end_data['url'])
+            db.session.add(end)
+        game.end = end
+
+        # Update route: delete old entries and add new ones
+        Route.query.filter_by(game_id=game.id).delete()  # Clear existing route entries
+        for title, url in zip(data['route']['titles'], data['route']['urls']):
+            new_route_entry = Route(game_id=game.id, title=title, url=url)
+            db.session.add(new_route_entry)
+
+        # Update other fields
+        game.fandom_domain = data['fandom_domain']
+        game.route_length = data['route_length']
+
+        # Commit changes
+        db.session.commit()
+        return make_response(jsonify({'message': f'game {id} updated'}), 200)
 
     except Exception as e:
-        return make_response(jsonify({'message':f'error getting game {id}', 'error':str(e)}), 500)
+        db.session.rollback()  # Rollback in case of error
+        return make_response(jsonify({'message': f'error updating game {id}', 'error': str(e)}), 500)
+
 
 # DELETE GAME(S)::
-
 @app.route('/api/game/<id>', methods=['DELETE'])
 def delete_game_by_id(id):
     try:
         game = Game.query.filter_by(id=id).first()
 
-        if game:
-            db.session.delete(game)
-            db.session.commit()
-            return make_response(jsonify({'message':f'game {id} was deleted'}), 200)
+        if not game:
+            return make_response(jsonify({'message': f'game {id} does not exist'}), 404)
 
-        return make_response(jsonify({'message':f'game {id} does not exit'}), 404)
+        # Delete the game (cascades to route entries)
+        db.session.delete(game)
+        db.session.commit()
+        return make_response(jsonify({'message': f'game {id} was deleted'}), 200)
 
     except Exception as e:
-        return make_response(jsonify({'message':f'error deleting game {id}', 'error':str(e)}), 500)
+        db.session.rollback()  # Rollback in case of error
+        return make_response(jsonify({'message': f'error deleting game {id}', 'error': str(e)}), 500)
+
 
 ########################################################################################################################
 # SAMPLE CRUD ROUTES::
